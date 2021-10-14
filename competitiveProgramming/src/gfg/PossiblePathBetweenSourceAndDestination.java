8e7ef122dcd6c3b100e2da0a9182445756de9fe9
Satyam Singh <satyamsinghkv2@gmail.com>
2021-10-14 13:27:42 +0530

    total number of possible path bitween source and destination in Graph (undirected)

--- competitiveProgramming/src/gfg/PossiblePathBetweenSourceAndDestination.java
+++ competitiveProgramming/src/gfg/PossiblePathBetweenSourceAndDestination.java
@@ -1,10 +1,25 @@
 package gfg;
-
+import java.util.*;
 public class PossiblePathBetweenSourceAndDestination {
-
+	static int count =0 ;
+    public static int countPaths(int V, ArrayList<ArrayList<Integer>> adj, int source,
+                   int destination) {
+        dfs(adj, source, destination);
+        return count;
+    }
+    public static void dfs(ArrayList<ArrayList<Integer>> adj, int source,
+                   int destination){
+        if(source == destination){
+            count++;
+            return;
+        }
+        for(int i: adj.get(source)){
+            dfs(adj, i, destination);
+        }
+        
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
